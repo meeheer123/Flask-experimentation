3cacc49f15ecc8b33f6f566e117e5f8ef418f8be
Mihir Pande <127537487+meeheer123@users.noreply.github.com>
2023-03-16 07:57:27 +0000

    used sqlalchemy

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request
-import pymysql
+from sqlalchemy import create_engine
 
+connectionString = "mysql+pymysql://8sevrae90cwiz17mpil5:pscale_pw_P7H8NoHsW6xToDrIYTp3ai0PwIWVx905B61PYeLcI2c@ap-south.connect.psdb.cloud/python_flask_website?charset=utf8mb4"
 
-def sqlConnector():
-  conn = pymysql.connect(
-    user='8sevrae90cwiz17mpil5',
-    password='pscale_pw_P7H8NoHsW6xToDrIYTp3ai0PwIWVx905B61PYeLcI2c',
-    db='python_flask_website',
-    host='ap-south.connect.psdb.cloud')
-  c = conn.cursor()
-  return conn, c
-
+engine = create_engine(connectionString,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 app = Flask(__name__)
 
@@ -20,11 +16,12 @@ def home():
   if request.method == 'POST':
     Name = request.form.get('batsman')
     Runs = request.form.get('runs')
-    conn, c = sqlConnector()
-    c.execute("INSERT INTO Data VALUES ('{}',{})".format(Name, int(Runs)))
-    conn.commit()
-    conn.close()
-    c.close()
+    Runs = int(Runs)
+    with engine.connect() as conn:
+      statement = f"INSERT INTO data (Name, Runs) VALUES ({Name}, {Runs})"
+      print(statement)
+      conn.execute(statement)
+      conn.commit()
   return render_template('home.html')
 
 
