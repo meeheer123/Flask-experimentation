0eb1ddf0f932426ad1ed962b02776bbaa5b2c339
Mihir Pande <127537487+meeheer123@users.noreply.github.com>
2023-03-16 11:39:06 +0000

    finished

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 connectionString = "mysql+pymysql://8sevrae90cwiz17mpil5:pscale_pw_P7H8NoHsW6xToDrIYTp3ai0PwIWVx905B61PYeLcI2c@ap-south.connect.psdb.cloud/python_flask_website?charset=utf8mb4"
 
@@ -14,16 +14,17 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
   if request.method == 'POST':
-    Name = request.form.get('batsman')
-    Runs = request.form.get('runs')
+    Name = request.form.get('name')
+    Runs = request.form.get('id')
+    Name = str(Name)
     Runs = int(Runs)
     with engine.connect() as conn:
-      statement = f"INSERT INTO data (Name, Runs) VALUES ({Name}, {Runs})"
-      print(statement)
-      conn.execute(statement)
+      conn.execute(text(
+        f"INSERT INTO Data (Name,Runs) VALUES ('{Name}',{Runs})"))
       conn.commit()
-  return render_template('home.html')
+  return render_template('registration.html')
 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug="True")
+
