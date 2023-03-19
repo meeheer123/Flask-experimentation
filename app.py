ce64fdbd62d7c1918d64680f98cc6f7035549906
Mihir Pande <127537487+meeheer123@users.noreply.github.com>
2023-03-19 14:00:02 +0000

    fixed the error

--- app.py
+++ app.py
@@ -13,40 +13,36 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def signIn():
-  if request.method == 'POST':
-    signInEmail = request.form.get('signInEmail')
-    signInPassword = request.form.get('signInPassword')
-    signInEmail = str(signInEmail)
-    signInPassword = str(signInPassword)
-
-    # Check if user exists in 'signUpDetails' table
-    with engine.connect() as conn:
-      result = conn.execute(
+    if request.method == 'POST':
+        if 'signInEmail' in request.form:
+            signInEmail = request.form.get('signInEmail')
+            signInPassword = request.form.get('signInPassword')
+            signInEmail = str(signInEmail)
+            signInPassword = str(signInPassword)
+            with engine.connect() as conn:
+              result = conn.execute(
         text(
           f"SELECT * FROM signUpDetails WHERE email='{signInEmail}' AND password='{signInPassword}'"
         ))
-      user = result.fetchone()
-      if user is None:
-        return render_template('registration_page.html',
+              user = result.fetchone()
+              if user is None:
+                return render_template('registration_page.html',
                                error_message="Invalid Email or Password")
 
-    signUpName = request.form.get('signUpName')
-    signUpEmail = request.form.get('signUpEmail')
-    signUpPassword = request.form.get('signUpPassword')
-    signUpName = str(signUpName)
-    signUpEmail = str(signUpEmail)
-    signUpPassword = str(signUpPassword)
-
-    # INSERT sign-up details into 'signUpDetails' table
-    with engine.connect() as conn:
-      conn.execute(
-        text(
-          f"INSERT INTO signUpDetails (name,email,password) VALUES ('{signUpName}','{signUpEmail}','{signUpPassword}')"
-        ))
-      conn.commit()
-
-  return render_template('registration_page.html')
-
+            conn.commit()
+        elif 'signUpName' in request.form:
+            signUpName = request.form.get('signUpName')
+            signUpEmail = request.form.get('signUpEmail')
+            signUpPassword = request.form.get('signUpPassword')
+            signUpName = str(signUpName)
+            signUpEmail = str(signUpEmail)
+            signUpPassword = str(signUpPassword)
+            with engine.connect() as conn:
+                conn.execute(
+                    text(
+                        f"INSERT INTO python_flask_website.signUpDetails (name,email,password) VALUES ('{signUpName}','{signUpEmail}','{signUpPassword}')"
+                    ))
+    return render_template('registration_page.html')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug="True")
+    app.run(host='0.0.0.0', debug="True")
